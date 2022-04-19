5f6537e793fd223cf5cb8131e3cbb341d5ffbb86
Kadi <gueneykadircan@gmail.com>
2022-04-19 13:40:29 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let headline = document.getElementById('headline');
+// console.log(überschrift)
+
+let liste = document.getElementById('bucketlist');
+// console.log(liste)
+
+function changeHeadline() {
+    // console.log("headline");
+    headline.classList.add('headline');
+}
+
+function changeBucket() {
+    // console.log("bucketlist");
+    bucketlist.classList.add('bucketlist');
+}
+
